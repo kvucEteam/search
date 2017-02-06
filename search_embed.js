2440e73adea9b5da40b5892f35255f4f3c175317
Anders Tornøe <ato@kvuc.dk>
2017-02-06 16:03:50 +0100

    embed (virker) + about html side oppe

---
+++ search_embed.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    console.log("embed up and running!");
+
+    $(".btn-search_embed").click(function() {
+        linktoSearchSite();
+    });
+
+    $(".closeinput").click(function() {
+        $(".search_textfield").val("");
+    });
+
+    $(document).on("keypress", function(e) {
+        // use e.which
+        if (e.keyCode == 13) {
+            linktoSearchSite();
+        }
+    });
+});
+
+
+
+function linktoSearchSite() {
+    var search_string = $(".search_textfield").val();
+
+    search_string = search_string.split(' ').join('+');
+
+
+    console.log("clicked search: " + search_string);
+
+    var searchURL = "https://www.vucdigital.dk/search?q=" + search_string;
+
+    window.location.href = searchURL;
+}
